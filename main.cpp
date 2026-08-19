1319cb2076798c626d8d0edacbb493872b60ae8e
mellie <babimelissa3@gmail.com>
2026-08-19 09:49:26 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,17 +8,40 @@
 #include <functional>
 #include <string>
 #pragma comment(lib, "ws2_32.lib")//window
- using namespace std ;bool sendInfo=false;;
-void recvrobotinfo(bool &sendInfo){
+ using namespace std ;
+ bool sendInfo=false;
+ //declaration
+ void recvrobotinfo(bool &sendInfo);
+ void sendcommand(bool &sendInfo);
+int main(){
+    std::cout << "Control Station starting..." << std::endl;
+//window
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);//window
 
+
+cout << "=================================\n";
+cout << "     ROBOT CONTROL STATION\n";
+cout << "=================================\n";
+
+//thread
+thread sen(sendcommand,ref(sendInfo));
+thread rcv(recvrobotinfo,ref(sendInfo) );
+sen.join();
+rcv.join();
+
+    return 0;
+}
+void recvrobotinfo(bool &sendInfo){
+// socket
  SOCKET udpSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-    sockaddr_in udp{};// socket bind
+    sockaddr_in udp{};
 
-    udp.sin_family = AF_INET;// socket bind
+    udp.sin_family = AF_INET;
     udp.sin_port = htons(5001);// socket bind
-    inet_pton(AF_INET, "192.168.100.34", &udp.sin_addr);
-
+    inet_pton(AF_INET, "172.20.10.4", &udp.sin_addr);
+// socket bind
 if (bind(udpSocket,(sockaddr*)&udp ,sizeof(udp)) != SOCKET_ERROR)
 
     std::cout << "UDP Bind successful!" << std::endl;
@@ -58,7 +81,7 @@ void sendcommand(bool &sendInfo){
 
     server.sin_family = AF_INET;// socket bind
     server.sin_port = htons(5000);// socket bind
-    inet_pton(AF_INET, "192.168.100.34", &server.sin_addr);// socket bind
+    inet_pton(AF_INET, "172.20.10.x4", &server.sin_addr);// socket bind
 
     if (bind(serverSocket,(sockaddr*)&server ,sizeof(server)) != SOCKET_ERROR)
 
@@ -69,14 +92,20 @@ void sendcommand(bool &sendInfo){
     if (listen(serverSocket, 2) != SOCKET_ERROR)
 
     std::cout << "Server is listening..." << std::endl;
-
-/*SOCKET robotSocket2 = accept(serverSocket, nullptr, nullptr);
-cout << "Robot 2 connected!" << endl;*/
-
+SOCKET robotSocket[100];
+//robot1
 SOCKET robotSocket1 = accept(serverSocket, nullptr, nullptr);
-cout << "Robot 1 connected!" << endl;
+if(robotSocket1!= SOCKET_ERROR)cout << "Robot 1 connected!" << endl;
+robotSocket[0]=robotSocket1;
+//robot2
+SOCKET robotSocket2 = accept(serverSocket, nullptr, nullptr);
+if(robotSocket2!= SOCKET_ERROR)cout << "Robot 2 connected!" << endl;
+robotSocket[1]=robotSocket2;
 
-while(true){int id ; id=0;
+
+
+while(true){
+int id ; id=0;
 int choice; choice=0;sendInfo=false;
 cout << "=============MENU=============\n";
 cout << "1-enter command \n";
@@ -84,73 +113,29 @@ cout << "2-exit\n";
 cin>>choice;
 
 if(choice==1){
-
+//sending commands
   cout<<"enter the robot id ";cin>>id;
   this_thread::sleep_for(chrono::seconds(1));
   string message2;
   cout<<"enter the command ";cin.ignore();getline(cin,message2);
-  send(robotSocket1,message2.c_str(),message2.size(),0);sendInfo=true;
+  send(robotSocket[id-1],message2.c_str(),message2.size(),0);
+  this_thread::sleep_for(chrono::seconds(1));
   cout<<"\n";
   cout<<"command to Robot "<<id<<" sent  : "<<message2;
-  cout<<"\n";
+  cout<<"\n";sendInfo=true;
   this_thread::sleep_for(chrono::seconds(2));
 
 }
 
 else if(choice==2){
-
+//quitting
   const char*message3="done\n";
   send(robotSocket1,message3,strlen(message3),0);
+  send(robotSocket2,message3,strlen(message3),0);
   cout<<"\n";
   cout<<"command sent  : "<<message3;
   this_thread::sleep_for(chrono::seconds(1));
   cout<<"\n communication finished\n "; break;}
 }
 
-/*
-const char*message1="demarrer\n";
-  send(robotSocket2,message1,strlen(message1),0);
-  cout<<"\n";
-  cout<<"command to Robot 2 sent  : "<<message1;
-  cout<<"\n";
- this_thread::sleep_for(chrono::seconds(1));
-const char*message4="arreter\n";
-  send(robotSocket1,message4,strlen(message4),0);
-  cout<<"\n";
-  cout<<"command to Robot 1 sent  : "<<message4;
-  cout<<"\n";
-  this_thread::sleep_for(chrono::seconds(1));
-const char*message5="changer la vitesse\n";
-  send(robotSocket2,message5,strlen(message5),0);
-  cout<<"\n";
-  cout<<"command to Robot 2 sent  : "<<message5;
-  cout<<"\n";
-  this_thread::sleep_for(chrono::seconds(1));
-const char*message3="done\n";
-  send(robotSocket1,message3,strlen(message3),0);
-  send(robotSocket2,message3,strlen(message3),0);
-  cout<<"\n";
-  cout<<"command sent  : "<<message3;
-  this_thread::sleep_for(chrono::seconds(1));
-  cout<<"\n communication finished\n ";
-*/
   }
-int main(){
-    std::cout << "Control Station starting..." << std::endl;
-
-    WSADATA wsaData;//window
-    WSAStartup(MAKEWORD(2, 2), &wsaData);//window
-
-
-cout << "=================================\n";
-cout << "     ROBOT CONTROL STATION\n";
-cout << "=================================\n";
-
-
-thread sen(sendcommand,ref(sendInfo));
-thread rcv(recvrobotinfo,ref(sendInfo) );
-sen.join();
-rcv.join();
-
-    return 0;
-}
