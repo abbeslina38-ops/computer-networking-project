b6a0e39f0d06dd32c1c0a1ffbf5f395a4c62b850
mellie <babimelissa3@gmail.com>
2026-09-09 13:04:19 +0100

    Update robot.h

--- robot.h
+++ robot.h
@@ -1,17 +1,15 @@
-#include "Robot.h"
+#ifndef ROBOT_H
+#define ROBOT_H
+#include <string>
+using namespace std;
 
-Robot::Robot()
-{
-
-}
-void Robot::sett(int i,float xx,float yy ,float v,string s){
-id=i;
-velocity=v;
-state=s;
-x=xx;
-y=yy;
-}
-Robot::~Robot()
-{
-    //dtor
-}
+class Robot{
+public:
+string  state;
+float x,y,velocity;
+int id;
+Robot();
+void sett(int i,float xx,float yy ,float v,string s);
+~Robot();
+};
+#endif // ROBOT_H
